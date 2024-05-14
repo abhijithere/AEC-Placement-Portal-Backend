4a7270c6b98406f5d1e8f3163e2fdc93f1d7ec92
Abhijit <sinhaabhijit2002@gmail.com>
2024-05-14 19:15:00 +0530

    yup1

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,12 @@ import { Teacher } from "../models/teacher.js";
 import  Jwt  from "jsonwebtoken";
 import ErrorHandler from "./error.js";
 export const isAuthenticated = async (req,res,next)=>{
-    const {token}= req.cookies;
-    if(!token) return res.status(404).json({
+    const {token}= await req.cookies;
+    if(!token) { return res.status(404).json({
      success:false,
      message:"login first",
      });
+    }
 
  const decoded = Jwt.verify(token,process.env.JWT_SECRET);
   req.user = await User.findById(decoded._id);
